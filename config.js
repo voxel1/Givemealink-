732215ef0ead11152d193bedc2c86b81a53ffc3a
Yea <129461157+voxel1@users.noreply.github.com>
2024-03-06 15:19:52 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  users: { // You can add multiple users by doing username: 'password'. 
+    Yippe: 'Nottoday',
+    guest: 'guestaccount134',
+  },
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
+}
+export default config
