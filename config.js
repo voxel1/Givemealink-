df9d846d2eee537df182ecc5884a2a7f2becbdc5
Yea <129461157+voxel1@users.noreply.github.com>
2024-03-06 15:35:01 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const config = {
+const config = 
   challenge: true, // Set to true if you want to enable password protection.
   users: { // You can add multiple users by doing username: 'password'. 
     Yippe: 'Mine',
